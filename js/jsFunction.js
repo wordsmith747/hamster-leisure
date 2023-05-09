171492b6d771eaf6f50d4d860fed412caa0cb8cc
wordsmith747 <89709407+wordsmith747@users.noreply.github.com>
2023-05-09 18:56:15 +0200

    Create registerButtonHandler function.

---
+++ js/jsFunction.js
@@ -0,0 +1,11 @@
+
+function registerButtonHandler() {
+    document.getElementById("js-1").onclick = function jsFunction() {
+        document.getElementById("js-1").style.backgroundColor = "Purple";
+
+    }
+    document.getElementById("js-2").onclick = function jsFunction() {
+        document.getElementById("js-2").style.backgroundColor = "Green";
+    }
+}
+
